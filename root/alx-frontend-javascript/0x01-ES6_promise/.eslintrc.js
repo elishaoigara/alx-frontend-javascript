73b9239f4d8680dfe23cad22133eb2374e6726ac
elishaoigara <elishaoigara50@gmail.com>
2025-04-21 17:05:27 +0000

    task1

---
+++ root/alx-frontend-javascript/0x01-ES6_promise/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
+
